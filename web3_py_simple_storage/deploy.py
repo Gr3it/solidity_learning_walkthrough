93c92255a15bfb9423063f4f0a89399720355e1a
unknown <emanuele.zini11@gmail.com>
2021-11-23 22:56:22 +0100

    commit

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -40,9 +40,9 @@ abi = json.loads(
 )["output"]["abi"]
 
 #for connetting to ganache
-w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
-chain_id = 5777
-my_address = "0xDBd5c32F90e4B7E8E3A768611defcFDb0Ca40FD1"
+w3 = Web3(Web3.HTTPProvider("https://rinkeby.infura.io/v3/d73bd5c41583447ea0b83243c3c519d1"))
+chain_id = 4
+my_address = "0x2F79c1ae4d60Bb2DfF0389782359E3676712e6E3"
 private_key = os.getenv("PRIVATE_KEY")
 
 #create the contract in python
@@ -60,5 +60,24 @@ transaction = SimpleStorage.constructor().buildTransaction(
 )
 
 signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+# Working with deployed Contracts
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+print(f"Initial Stored Value {simple_storage.functions.retrieve().call()}")
+greeting_transaction = simple_storage.functions.store(50).buildTransaction(
+    {"chainId": chain_id, "from": my_address, "nonce": nonce + 1}
+)
+signed_greeting_txn = w3.eth.account.sign_transaction(
+    greeting_transaction, private_key=private_key
+)
+tx_greeting_hash = w3.eth.send_raw_transaction(signed_greeting_txn.rawTransaction)
+print("Updating stored Value...")
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_greeting_hash)
+
+print(simple_storage.functions.retrieve().call())
 
 
